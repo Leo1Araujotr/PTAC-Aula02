a6adf00258691819d26425ca3e13696d5afb7978
Leo1Araujotr <leonardoaraujo44643@gmail.com>
2024-03-22 16:07:49 -0400

    atualização do dia 22/03

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,16 +2,31 @@ import { useState } from "react";
 
 export default function Main(){
     const [nome, setNome] = useState("");
-    const [Number, setNumber] = useState(0)
+    const [telefone, setTelefone] = useState(0)
+    const [listaContatos, setListaContatos] = useState([]);
+    
+    const registrar = () =>{
+        event.preventDefault();
+       alert("olá")
+       setListaContatos([...listaContatos,
+    {
+        nomeSalvo: nome,
+        telefoneSalvo: telefone,
+    }
+    
+    ]);
+    };
+
     return(
     <main>
-        <form>
-          <input type="text" name="" id="" onChange={(event) => setNome(event.target.value)}/>
+        <form action="" onSubmit={registrar}>
+        <label htmlFor="nome">Nome:</label>  <input type="text" name="" id="nome" onChange={(event) => setNome(event.target.value)}/>
           {nome}
 
       
-          <input type="text" name=""  id="" onChange={(event) => setNumber(event.target.value)}/>
-          {Number}
+         <label htmlFor="NumeroTelefone">N. Telefone:</label> <input type="text" telefone-contato=""  id="telefone" onChange={(event) => setTelefone(event.target.value)}/>
+          {telefone}
+         <button>Enviar</button>
          </form>
     </main>
     );
